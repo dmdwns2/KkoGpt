a8cd580b7539c3d87ce91b89cd5d30cf6c948bef
EungJun <ssreki1@naver.com>
2025-04-19 06:33:34 +0900

    fix: #2 adjust JSON response format

--- src/main/java/com/example/kkogpt/controller/api/KkoController.java
+++ src/main/java/com/example/kkogpt/controller/api/KkoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,16 +18,14 @@ public class KkoController {
     public ResponseEntity<Map<String, Object>> skill(@RequestBody Map<String, Object> payload) {
         log.info("[kakao] payload : " + payload);
 
-        Map<String, Object> response = Map.of(
-                "version", "2.0",
-                "template", Map.of(
-                        "outputs", List.of(
-                                Map.of("simpleText", Map.of(
-                                        "text", "hi"
-                                ))
-                        )
-                )
-        );
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("version", "2.0");
+
+        Map<String, Object> simpleText = Map.of("text", "hi");
+        Map<String, Object> output = Map.of("simpleText", simpleText);
+        Map<String, Object> template = Map.of("outputs", List.of(output));
+
+        response.put("template", template);
         log.info("[success] skill");
         return ResponseEntity.ok(response);
     }
