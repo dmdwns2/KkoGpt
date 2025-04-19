bd652ae9f07dd6fb1785aac2fb3aa8bc78dc704c
EungJun <ssreki1@naver.com>
2025-04-20 01:43:34 +0900

    chore: update shell

--- start.sh
+++ start.sh
@@ -7,6 +7,19 @@ set +o allexport
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] [success] env loaded "
 
-nohup java -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+APP_NAME=demo-0.0.1-SNAPSHOT.jar
+LOG_FILE=app.log
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Spring Boot started "
+PID=$(pgrep -f $APP_NAME)
+if [ -n "$PID" ]; then
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] [stopped] old process killed (PID=$PID)..."
+  kill -15 $PID
+  sleep 3
+else
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] [info] no running process found"
+fi
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] [starting] launching Spring Boot"
+nohup java -jar ./$APP_NAME > $LOG_FILE 2>&1 &
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] [success] Spring Boot started (log: $LOG_FILE)"
