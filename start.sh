693cbb6269e98c67eeb500865577e98e41cb695d
EungJun <ssreki1@naver.com>
2025-04-20 00:18:35 +0900

    chore: update shell

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ set +o allexport
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] [success] env loaded "
 
-nohup java -jar ./build/libs/myapp-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+nohup java -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] Spring Boot started "
